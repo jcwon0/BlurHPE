58a7d1dd594949ae04d66356cf121d2e73a309dc
jcwon <jcwon@postech.ac.kr>
2021-04-03 05:06:51 +0900

    Add bottom up posetrack

--- mmpose/datasets/datasets/bottom_up/__init__.py
+++ mmpose/datasets/datasets/bottom_up/__init__.py
@@ -2,8 +2,9 @@ from .bottom_up_aic import BottomUpAicDataset
 from .bottom_up_coco import BottomUpCocoDataset
 from .bottom_up_crowdpose import BottomUpCrowdPoseDataset
 from .bottom_up_mhp import BottomUpMhpDataset
+from .bottom_up_posetrack import BottomUpPoseTrack18Dataset
 
 __all__ = [
     'BottomUpCocoDataset', 'BottomUpCrowdPoseDataset', 'BottomUpMhpDataset',
-    'BottomUpAicDataset'
+    'BottomUpAicDataset', 'BottomUpPoseTrack18Dataset'
 ]
